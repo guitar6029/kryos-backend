f63fb94b53ad37d100f71430b279af29601f4dd3
jsdev305 <jsdev305@gmail.com>
2025-12-29 22:04:09 -0500

    added pg pool

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import "dotenv/config";
+import { pool } from "./db.js";
 import app from "./app.js";
 
 const PORT = Number(process.env.PORT) || 3000;
+
+await pool.query("SELECT 1");
+console.log("Database connected")
+
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT : ${PORT}`);
