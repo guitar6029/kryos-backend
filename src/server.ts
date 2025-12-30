ad40b48fc5d5feb4a8fbd24c6af75837afb24b5f
jsdev305 <jsdev305@gmail.com>
2025-12-29 21:18:58 -0500

    infra: add Docker, Docker Compose, and dotenv configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import app from "./app.js";
 
-const PORT = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT : ${PORT}`)
-})
+  console.log(`Listening on PORT : ${PORT}`);
+});
