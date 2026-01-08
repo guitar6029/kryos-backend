ad2d5ca89535275e6d8284081047a57b256aae4e
jsdev305 <jsdev305@gmail.com>
2026-01-08 11:54:13 -0500

    feat(db): add users table for authentication

---
+++ migrations/1767889118909_users-table-create.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "serial",
+      primaryKey: true,
+    },
+    name: {
+      type: "text",
+      notNull: true,
+    },
+    email: {
+      type: "text",
+      unique: true,
+      notNull: true,
+    },
+    password_hash: {
+      type: "text",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("users");
+};
